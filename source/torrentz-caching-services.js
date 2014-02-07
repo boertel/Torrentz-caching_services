7047ce95e197b1d55ad3125afba6f8698b49bf79
boertel <ben@punchtab.com>
2014-02-07 14:15:31 -0800

    float: left the numbers and add zoink to the list

--- source/torrentz-caching-services.js
+++ source/torrentz-caching-services.js
@@ -1,6 +1,11 @@
 var selector = ".results dl a",
-    nodes = Array.prototype.slice.call(document.querySelectorAll(selector), 0),
+    slice = function (nodes) { return Array.prototype.slice.call(nodes, 0); },
+    torrents = slice(document.querySelectorAll(selector)),
+    dds = slice(document.querySelectorAll(".results dd")),
     services = {
+        zoink: function (h) {
+            return 'http://zoink.it/torrent/' + h.toUpperCase() + '.torrent';
+        },
         torrage: function (h) {
             return 'http://torrage.com/torrent/' + h.toUpperCase() + '.torrent';
         },
@@ -11,24 +16,62 @@ var selector = ".results dl a",
             return 'magnet:?xt=urn:btih:' + h;
         }
     },
-    active = ['torrage', 'magnet', 'torcache'];
+    active = ['torrage', 'magnet', 'zoink', 'torcache'];
 
-nodes.forEach(function (node) {
-    var hash = node.getAttribute('href').replace('/', '');
-    var container = document.createElement("span");
 
-    active.forEach(function (name) {
-        var service = services[name];
-        if (service) {
-            var dash = document.createTextNode(" - ");
-            container.appendChild(dash);
 
-            var a = document.createElement('a');
-            a.href = service(hash);
-            a.innerHTML = name[0].toUpperCase() + name[1] + name[2];
-            container.appendChild(a);
-        }
+var internals = {};
+
+internals.css = function () {
+    var sheet = window.document.styleSheets[0];
+    sheet.addRule("div.results>dl>dt", "width: auto");
+    sheet.addRule("div.results>dl>dt", "margin-left: 330px");
+    sheet.addRule("div.results>dl>dt", "overflow-x: visible");
+    sheet.addRule("div.results>dl>dd", "float: left");
+    sheet.addRule("div.results>dl>dd", "width: 330px");
+    sheet.addRule("div.results>dl>dd>span.t", "width: 50px");
+    sheet.addRule("div.results>dl>dd>span.t", "color: #75808A");
+};
+
+internals.sum = function (nodes) {
+    nodes.forEach(function (node) {
+        var peers = slice(node.querySelectorAll("span.u, span.d")).map(function (d) {
+            return parseInt(d.innerHTML, 10);
+        });
+        var sum = peers.reduce(function (prev, current) {
+            return prev + current;
+        });
+
+        var span = document.createElement("span");
+        span.className = "t";
+        span.innerHTML = sum;
+        node.appendChild(span);
     });
+};
 
-    node.parentNode.appendChild(container);
-});
+
+internals.links = function (nodes) {
+    nodes.forEach(function (node) {
+        var hash = node.getAttribute('href').replace('/', '');
+        var container = document.createElement("span");
+
+        active.forEach(function (name) {
+            var service = services[name];
+            if (service) {
+                var dash = document.createTextNode(" - ");
+                container.appendChild(dash);
+
+                var a = document.createElement('a');
+                a.href = service(hash);
+                a.innerHTML = name[0].toUpperCase() + name[1] + name[2];
+                container.appendChild(a);
+            }
+        });
+
+        node.parentNode.appendChild(container);
+    });
+};
+
+internals.css();
+internals.links(torrents);
+internals.sum(dds);
